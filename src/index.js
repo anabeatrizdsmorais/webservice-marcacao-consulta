39a38075351b4ed6505e379aa1ff67711a1c9b60
Ana B Morais <anabeatrizdsmorais@gmail.com>
2023-10-31 01:33:20 -0300

    feat: adicao de mais dados no json e adicao do menu de adicionar consulta

--- src/index.js
+++ src/index.js
@@ -1,7 +1,108 @@
 
 const btnShowDisponiveis = document.getElementById('disponiveis');
 const btnShow = document.getElementById('agendadas');
+const btnAgendar = document.getElementById('agendar');
 
+btnAgendar.addEventListener('click', function(){
+    
+    fetch('./json/data.json'/* , {method: 'GET'} */)
+    .then(response => {
+        if (!response.ok) {
+          throw new Error('Erro ao obter os dados');
+        }
+        return response.json();
+    })
+    .then(data => {
+        const array = data.consultas;
+
+        let html = ``;
+        let styleSelect = ` style=" width: 400px; height: 30px; border-radius: 5px; border-color: #cacaca; margin-bottom: 15px; " `;
+        let styleButton = ` style=" width: 100px; height: 30px; border-radius: 5px; border: none; cursor: pointer; background-color: #011d1d; color: #fff; "`;
+
+        html += `<h3 style="color: #121212 ">Cadastro de consulta</h3> <br><br>`;
+        html += `<form action="" method="post">`;
+        
+        html += `<div>`;
+        html += `<label>Especialidade, médico e data/hora </label> <br><br>`;
+        html += `<select ${styleSelect} id="selectDados">`;
+        html += `<option value="">Selecione</option>`;
+        for (let index = 0; index < array.length; index++) {
+            let element = array[index];
+            if(element.disponivel){
+                html += `<option value="${element.especialidade}#${element.medico}#${element.data}#${element.id}">${element.especialidade} - ${element.medico} - ${element.data}</option>`;
+            }
+        }
+        html += `</select>`;
+        html += `</div>`;
+        
+        // html += `<div>`;
+        // html += `<label>Médico </label> <br>`;
+        // html += `<select ${styleSelect}>`;
+        // html += `<option>Selecione o médico</option>`;
+        // for (let index = 0; index < array.length; index++) {
+        //     let element = array[index];
+        //     if(element.disponivel){
+        //         html += `<option value="${element.medico}">${element.medico}</option>`;
+        //     }
+        // }
+        // html += `</select>`;
+        // html += `</div>`;
+
+        // html += `<div>`;
+        // html += `<label>Data e hora </label> <br>`;
+        // html += `<select ${styleSelect}>`;
+        // html += `<option>Selecione a data e a hora disponível</option>`;
+        // for (let index = 0; index < array.length; index++) {
+        //     let element = array[index];
+        //     if(element.disponivel){
+        //         html += `<option value="${element.data}">${element.data}</option>`;
+        //     }
+        // }
+        // html += `</select>`;
+        // html += `</div>`;
+        
+        html += `<div>`;
+        html += `<button type="button" ${styleButton} id="salvar"> Salvar </button>`;
+        html += `</div>`;
+        
+        html += `</form>`;
+
+        document.querySelector('#consultas').innerHTML = html;
+
+        const btnSalvar = document.querySelectorAll('#salvar');
+        btnSalvar.forEach(salvar => {
+            salvar.addEventListener('click', function() {
+                const selectElement = document.getElementById('selectDados');
+                const selectedOption = selectElement.options[selectElement.selectedIndex];
+                const value = selectedOption.value;
+                const text = selectedOption.text;
+
+                // console.log('Valor selecionado:', value);
+
+                const partes = value.split("#");
+
+                const especialidade = partes[0];
+                const medico = partes[1];
+                const data_hora = partes[2];
+                const id_consulta = partes[3];
+                
+                let array = JSON.parse(sessionStorage.getItem("dados_consulta")) || []; 
+                array.push({id_consulta, medico, especialidade, data_hora});
+                sessionStorage.setItem("dados_consulta", JSON.stringify(array));
+
+                alert('Sucesso! Você acaba de marcar sua consulta. Vá no menu "Agendamentos" e verifique.');
+
+            });
+        });
+
+    })
+    .catch(error => {
+        console.error('Erro:', error);
+    });
+    
+});
+
+// Mostra consultas diponiveis
 btnShowDisponiveis.addEventListener('click', function(){
     
     fetch('./json/data.json'/* , {method: 'GET'} */)
@@ -49,9 +150,10 @@ btnShowDisponiveis.addEventListener('click', function(){
                 const medico = this.getAttribute('data-medico');
                 const especialidade = this.getAttribute('data-especialidade');
                 const data_hora = this.getAttribute('data-data_hora');
+                const paciente = "Victor Santos";
 
                 let array = JSON.parse(sessionStorage.getItem("dados_consulta")) || []; 
-                array.push({id_consulta, medico, especialidade, data_hora});
+                array.push({id_consulta, medico, especialidade, data_hora, paciente});
                 sessionStorage.setItem("dados_consulta", JSON.stringify(array));
 
                 const index = array.findIndex(element => element.id === id_consulta);
@@ -69,7 +171,7 @@ btnShowDisponiveis.addEventListener('click', function(){
     
 });
 
-
+//Mostra consultas agendadas
 btnShow.addEventListener('click', function(){
     
     fetch('./json/data.json'/* , {method: 'GET'} */)
@@ -83,16 +185,17 @@ btnShow.addEventListener('click', function(){
         const array = data.consultas;
        
         let dados = sessionStorage.getItem("dados_consulta");
+        let html = "";
 
         if (dados) {
             let array = JSON.parse(dados);
         
-            let html = "";
         
             array.forEach(consulta => {
 
                 html += `
                     <div style=" font-weight: bold; color: #6b0c19; text-decoration: none; ">
+                        <p>Paciente: Victor Santos</p>
                         <p>Especialidade: ${consulta.especialidade}</p>
                         <p>Médico: ${consulta.medico}</p>
                         <p>Data e Hora: ${consulta.data_hora}</p>
@@ -101,7 +204,10 @@ btnShow.addEventListener('click', function(){
                 `;
             });
             document.getElementById('consultas').innerHTML = html;
-        
+            
+        } else {
+            html += " Não há nada agendado. ";
+            document.getElementById('consultas').innerHTML = html;
         }
 
     })
